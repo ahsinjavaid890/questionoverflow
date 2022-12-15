f9e1c9cbf579c104a84004ded8f1ce2dbb44dd65
Usama Tariq <usamatariq9949@gmail.com>
2022-12-15 16:10:27 -0700

    full view and copy button done

--- resources/views/frontend/tutorials/tutorialdetail.blade.php
+++ resources/views/frontend/tutorials/tutorialdetail.blade.php
@@ -16,9 +16,13 @@
 @endsection
 @section('content')
 <style type="text/css">
-  .why-use ul{
-    margin-left: 20px;
-  }
+   .why-use ul {
+      margin-left: 20px;
+   }
+   .remove_overflow{
+    overflow: none;
+    max-height: 100%;
+}
 </style>
 <section class="hero-area pattern-bg-2 bg-white shadow-sm overflow-hidden pt-50px pb-50px">
    <div class="container-fluid">
@@ -48,65 +52,69 @@
                </div>
             </div>
             <div class="tutorialcontent">
-            @foreach(DB::table('tutorial_sections')->where('tutorial_id' , $data->id)->orderby('order' , 'ASC')->get() as $r)
-            <div class="server-side mt-3">
-               @if($r->heading)
-               <div class="why-use mt-3">
-                  <h5 class="font-weight-bold">{{$r->heading}}</h5>
-               </div>
-               @endif
-               @if($r->description)
-               <div class="why-use mt-3">
-                  {!! $r->description !!}
-               </div>
-               @endif
-               @if($r->content)
-               <div class="contentheading">
-                  <div class="row">
-                     <div class="col-md-6">
-                        @if($r->content_heading)
-                           <h5 class="text-white">{{ $r->content_heading  }}</h5>
-                        @endif
-                     </div>
-                     <div class="col-md-6 text-right text-white">
-                          <a class="text-white btn btn-sm btn-primary" href="javascript:void(0)"><i class="la la-clipboard-check"></i> Coppy </a>
-                          @if($r->content_file)
-                          <a class="text-white btn btn-sm btn-primary" href="javascript:void(0)"><i class="la la-download"></i> Download </a>
-                          @endif
-                          <a onclick="fullviewbutton({{$r->id}})" id="fullviewbutton{{$r->id}}" class="text-white btn btn-sm btn-primary" href="javascript:void(0)"><i class="la la-street-view"></i> Full View </a>
-                     </div>
+               @foreach(DB::table('tutorial_sections')->where('tutorial_id' , $data->id)->orderby('order' , 'ASC')->get() as $r)
+               <div class="server-side mt-3">
+                  @if($r->heading)
+                  <div class="why-use mt-3">
+                     <h5 class="font-weight-bold">{{$r->heading}}</h5>
                   </div>
-                  
+                  @endif
+                  @if($r->description)
+                  <div class="why-use mt-3">
+                     {!! $r->description !!}
+                  </div>
+                  @endif
+                  @if($r->content)
+                  <div class="contentheading">
+                     <div class="row">
+                        <div class="col-md-6">
+                           @if($r->content_heading)
+                           <h5 class="text-white">{{ $r->content_heading  }}</h5>
+                           @endif
+                        </div>
+                        <div class="col-md-6 text-right text-white">
+                           <a onclick="copycodes({{$r->id}})" id="copycodes{{$r->id}}" class="text-white btn btn-sm btn-primary"href="javascript:void(0)"><i class="la la-clipboard-check"></i> Coppy </a>
+                           @if($r->content_file)
+                           <a class="text-white btn btn-sm btn-primary" href="javascript:void(0)"><i class="la la-download"></i> Download </a>
+                           @endif
+                           <a onclick="fullviewbutton({{$r->id}})" id="fullviewbutton{{$r->id}}" class="text-white btn btn-sm btn-primary" href="javascript:void(0)"><i class="la la-street-view"></i> Full View </a>
+                        </div>
+                     </div>
+
+                  </div>
+                  <div class="contentcode" id="contentcode{{$r->id}}">
+                     {!! $r->content !!}
+                  </div>
+                  @endif
                </div>
-               <div class="contentcode">
-                  {!! $r->content !!}
-               </div>
-               @endif
-            </div>
-            @endforeach
+               @endforeach
             </div>
             <div class="downloadbuttons mt-3 d-flex flex-row-reverse">
-                  @if($data->demo)
-                      <a class="btn theme-btn theme-btn-outline mr-2 p-2" href=""><i class="la la-eye"></i> View Demo</a>
-                    @endif
-                    @if($data->dounloadlink)
-                      <a class="btn theme-btn theme-btn-outline mr-2 p-2" href=""><i class="la la-download"></i> Download Source Code</a>
-                    @endif
-                    <a class="btn theme-btn theme-btn-outline mr-2 p-2" href="{{ url('needhelp') }}/{{ $data->url }}">Need Help ?</a>
+               @if($data->demo)
+               <a class="btn theme-btn theme-btn-outline mr-2 p-2" href=""><i class="la la-eye"></i> View Demo</a>
+               @endif
+               @if($data->dounloadlink)
+               <a class="btn theme-btn theme-btn-outline mr-2 p-2" href=""><i class="la la-download"></i> Download Source Code</a>
+               @endif
+               <a class="btn theme-btn theme-btn-outline mr-2 p-2" href="{{ url('needhelp') }}/{{ $data->url }}">Need Help ?</a>
 
-                    <button class="btn facebook-btn theme-btn mr-2 p-2" type="button">
-                        <span class="btn-icon"><svg focusable="false" width="16px" height="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg></span>
-                        <span style="padding-left: 44px;" class="flex-grow-1">Share On Facebook</span>
-                    </button>
+               <button class="btn facebook-btn theme-btn mr-2 p-2" type="button">
+                  <span class="btn-icon"><svg focusable="false" width="16px" height="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
+                     </svg></span>
+                  <span style="padding-left: 44px;" class="flex-grow-1">Share On Facebook</span>
+               </button>
 
-                    <button class="btn twitter-btn theme-btn mr-2 p-2" type="button">
-                        <span class="btn-icon"><svg focusable="false" width="16px" height="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg></span>
-                        <span style="padding-left: 44px;" class="flex-grow-1">Share On Twitter</span>
-                    </button>
-                    <button class="btn instagram-btn theme-btn mr-2 p-2" type="button">
-                        <span class="btn-icon"><i class="la la-instagram mr-1"></i></span>
-                        <span style="padding-left: 44px;" class="flex-grow-1">Share On Instagram</span>
-                    </button>
+               <button class="btn twitter-btn theme-btn mr-2 p-2" type="button">
+                  <span class="btn-icon"><svg focusable="false" width="16px" height="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
+                     </svg></span>
+                  <span style="padding-left: 44px;" class="flex-grow-1">Share On Twitter</span>
+               </button>
+               <button class="btn instagram-btn theme-btn mr-2 p-2" type="button">
+                  <span class="btn-icon"><i class="la la-instagram mr-1"></i></span>
+                  <span style="padding-left: 44px;" class="flex-grow-1">Share On Instagram</span>
+               </button>
             </div>
             <div id="comments" class="tutorialcoments">
                @if(DB::table('tutorial_comments')->where('tutorial_id' , $data->id)->orderby('created_at' , 'desc')->count() > 0)
@@ -114,7 +122,7 @@
                @endif
                @foreach(DB::table('tutorial_comments')->where('tutorial_id' , $data->id)->orderby('created_at' , 'desc')->get() as $r)
                @php
-                  $user = DB::table('users')->where('id' , $r->user_id)->first();
+               $user = DB::table('users')->where('id' , $r->user_id)->first();
                @endphp
                <div class="card border-0 bg-white mt-3 tutorials-card">
                   <div class="card-body">
@@ -123,7 +131,7 @@
                         <img src="{{ url('public/images') }}/{{ $user->profileimage }}" alt="{{ $user->name }}" class="rounded-circle ">
                         @else
                         <img width="40" src="{{ url('public/images/profile-placeholder-question-overflow.png') }}" alt="Question Overflow" class="rounded-circle ">
-                        
+
                         @endif
                         <div class="ml-2">
                            <h6 class="font-weight-bold">{{ $user->name }}</h6>
@@ -154,9 +162,9 @@
                   </div>
                </div>
                @endforeach
-               
+
                <h4 class="heading mt-4 font-weight-bold border-bottom ">Leave a comment</h4>
-               
+
                <form method="POST" action="{{ url('tutorials/addcomment') }}" class=" mt-3 py-2 post-comment ">
                   @csrf
                   <input type="hidden" value="{{$data->id}}" name="tutorial_id">
@@ -191,15 +199,15 @@
             @if($data->youtubelink)
             <div class="row pr-3 sidebar-tutorial">
                <div class="col-md-12 mb-4 px-0  ">
-                 <div class="video-box">
+                  <div class="video-box">
                      <img class="w-100 rounded-rounded lazy" src="{{ url('public/images') }}/{{ $data->image }}" data-src="{{ url('public/images') }}/{{ $data->image }}" alt="video image">
                      <div class="video-content text-center">
-                         <a class="icon-element icon-element-lg hover-y mx-auto" href="https://www.youtube.com/watch?v=GlrxcuEDyF8" data-fancybox="" title="Play Video">
-                             <i class="la la-youtube mr-1"></i>
-                         </a>
-                         <p class="mt-2 badge badge-light">Watch Tutorial Video For {{ $data->name }}</p>
+                        <a class="icon-element icon-element-lg hover-y mx-auto" href="https://www.youtube.com/watch?v=GlrxcuEDyF8" data-fancybox="" title="Play Video">
+                           <i class="la la-youtube mr-1"></i>
+                        </a>
+                        <p class="mt-2 badge badge-light">Watch Tutorial Video For {{ $data->name }}</p>
                      </div>
-                 </div>
+                  </div>
                </div>
             </div>
             @endif
@@ -209,13 +217,31 @@
    </div>
 </section>
 <script>
-  $(document).ready(function(){
-    $(".user-reply1").hide(function(){
-      $(".reply-1").click(function(){
-        $(".user-reply1").fadeToggle() ;
-      });
-     
-    })
-  })
+   /// copy code ///
+function copycodes(id) {
+  const copyText = document.getElementById('contentcode' + id).textContent;
+  const textArea = document.createElement('textarea');
+  textArea.textContent = copyText;
+  document.body.append(textArea);
+  textArea.select();
+  document.execCommand("copy");
+}
+
+
+
+  /// full view ///
+  function fullviewbutton(id) {
+   const div = document.getElementById('contentcode' + id);
+   div.classList.toggle('remove_overflow');
+  }
+
+   $(document).ready(function() {
+      $(".user-reply1").hide(function() {
+         $(".reply-1").click(function() {
+            $(".user-reply1").fadeToggle();
+         });
+
+      })
+   })
 </script>
 @endsection
